79f2b2e889e8bbbeeca02ed5eebe4fd7429cadac
vanely <vanelys94@gmail.com>
2019-01-17 07:56:38 -0500

    user authentication info successfully being saved to database(no duplicates)

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 
-require('./models/user');//follows an order of operations(needs to be before passport config)
+//follows an order of operations(needs to be before passport config)
+require('./models/user');
 
 //importing passport config from services to make sure the config runs
 require('./services/passportConfig');
 
 //mongoose connection to Mlab
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI, {
+    //prevents deprecated parser error
+    useNewUrlParser: true
+});
+
 const app = express();
 
 //importing arrow function from authRoutes and passing the instance of express app as its param to allow express app to be used on the authRoutes
--- services/passportConfig.js
+++ services/passportConfig.js
@@ -1,12 +1,13 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const mongoose = require('mongoose');
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
 //the google-oauth module comes with several properties. We only want the (Strategy)
 
 //when model takes 2 args. We are creating/ loading a schema into mongoose.
 //when it takes one. We are fetching a schema
-const user = mongoose.model('users');
+//allways import a model into another file using this method to prevent accidental model duplication when testing.
+const User = mongoose.model('users');
 
 //connect passport to google strategy(create a new instance of the strategy) config
 //console.developers.google.com(oauth API will generate "clientID", and "clientSecret")
@@ -19,8 +20,24 @@ passport.use(
 
         (accessToken, refreshToken, profile, done) => {
             
-            //uses googleID property defined in schema and assigns it profile.id from authentication json and implements the save() method to save it to database
-            new user({googleID: profile.id}).save
+            User.findOne({googleID: profile.id})
+            .then((existingUser) => {
+                
+                if(existingUser) {
+                    
+                    //already have user in DB, return it with done()
+                    done(null, existingUser);
+                }
+                else {
+                    
+                    //uses googleID property defined in schema and assigns it profile.id from authentication json and implements the save() method to save it to database
+                    new User({googleID: profile.id})
+                    .save()
+                    .then((user) => {
+                        done((null, user)); 
+                    });
+                }
+            })
         }
     )
 );
